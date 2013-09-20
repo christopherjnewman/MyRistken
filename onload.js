30839e4110c28c4588683883af57ae65d645ab4e
miles <miles.okeefe@gmail.com>
2013-09-20 06:52:15 -0500

    My Applications page

--- onload.js
+++ onload.js
@@ -1,5 +1,9 @@
 $(function(){
 	function checkHash(){
+		//resets
+		$(".nav-item-selected").removeClass('nav-item-selected');
+		$(".modal").css('visibility', 'hidden');
+		$(".content").css('display', 'none');
 		if(location.hash == "#forgot"){
 			$(".modal").css('visibility', 'hidden');
 			$("#forgot-modal").css('visibility', 'visible');
@@ -13,8 +17,16 @@ $(function(){
 			$(".modal").css('visibility', 'hidden');
 			$("#new-password-success-modal").css('visibility', 'visible');
 		}else if(location.hash == "#dash"){
-			$(".modal").css('visibility', 'hidden');
-			$(".dash").css('visibility', 'visible');
+			$("#dashboard-link").addClass('nav-item-selected');
+			$("#nav").css('visibility', 'visible');
+			$("#apps").css('display', 'block');
+			$("#apps").css('visibility', 'visible');
+		}else if(location.hash == "#settings"){
+			//$(".dash").css('visibility', 'invisible');
+			$("#settings-link").addClass('nav-item-selected');
+			$("#nav").css('visibility', 'visible');
+			$("#settings").css('display', 'block');
+			$("#my-applications-table").dataTable();
 		}else{
 			$("#login-modal").css('visibility', 'visible');
 		}
@@ -72,5 +84,52 @@ $(function(){
 		  })
 		});
 	}
+	$(".app-wrapper").click(function(){
+		if(!$(this).hasClass('app-open')){ //is closed and is about to be opened
+			$(this).addClass('app-open');
+			var type = "svg";
+			if(!Modernizr.svg) //ie fallback
+				type = "png";
+			var $arrow = $(this).find(".app-arrow");
+			$arrow.attr('src', 'up-arrow.' + type);
+			//reset the width so that the new svg fits
+			var width = $arrow.width();
+			$arrow.width(0);
+			$arrow.width(width);
+			var id = $(this).attr('id');
+			$(".app-wrapper").each(function(){
+				if($(this).attr('id') != id){
+					$(this).addClass('faded');
+				}
+			});
+			if(id == "menu-app"){
+				/*var left = $(this).offset().left - 40;
+				var top = $(this).offset().top + $(this).outerHeight() + 30;
+				$("#select-dealership").css('left', left + "px");
+				$("#select-dealership").css('top', top + "px");*/
+				$("#select-dealership").removeClass("hidden");
+				setTimeout(function(){$("#select-dealership").addClass('tooltip-down');}, 50);
+				setTimeout(function(){$("#select-dealership").addClass('tooltip-expanded');}, 500);
+			}
+		}else{ //was open, about to be closed
+			$(this).removeClass('app-open');
 
+			var type = "svg";
+			if(!Modernizr.svg) //ie fallback
+				type = "png";
+			var $arrow = $(this).find(".app-arrow");
+			$arrow.attr('src', 'down-arrow.' + type);
+			//reset the width so that the new svg fits
+			var width = $arrow.width();
+			$arrow.width(0);
+			$arrow.width(width);
+			var id = $(this).attr('id');
+			$(".faded").each(function(){
+				$(this).removeClass('faded');
+			});
+				$("#select-dealership").removeClass('tooltip-expanded');
+				setTimeout(function(){$("#select-dealership").addClass('tooltip-down');}, 200);
+				setTimeout(function(){$("#select-dealership").addClass("hidden");}, 300);
+		}
+	});
 });
